a21be45affd2a8fd60fba5017fda5c2a21ad05e8
jesumb <jesus.moya.barajas00@gmail.com>
2021-12-13 09:54:50 +0100

    Añadida clase ticket

--- _com/Clases.php
+++ _com/Clases.php
@@ -170,3 +170,138 @@ class Persona extends Dato implements JsonSerializable
         return ($this->categoriaId == $categoria->getId());
     }
 }
+
+class Ticket extends Dato implements JsonSerializable
+{
+    use Identificable;
+
+    private string $cif;
+    private int $numeroTicket;
+    private date $fechaHora;
+    private string $lugar;
+    private int $caja;
+    private int $precio;
+    private bool $descuento;
+    private int $total;
+
+    private ?array $personasPertenecientes = null;
+
+    public function __construct(int $id, string $cif,int $numeroTicket,date $fechaHora,string $lugar,int $caja,int $precio,bool $descuento,int $total)
+    {
+        $this->id = $id;
+        $this->setCif($cif);
+        $this->setNumeroTicket($numeroTicket);
+        $this->setFechaHora($fechaHora);
+        $this->setLugar($lugar);
+        $this->setCaja($caja);
+        $this->setPrecio($precio);
+        $this->setDescuento($descuento);
+        $this->setTotal($total);
+    }
+
+    public function getCif(): string
+    {
+        return $this->cif;
+    }
+
+    public function setCif(string $cif)
+    {
+        $this->cif = $cif;
+    }
+    public function getNumeroTikect(): int
+    {
+        return $this->numeroTicket;
+    }
+
+    public function setNumeroTicket(int $numeroTicket)
+    {
+        $this->numeroTicket = $numeroTicket;
+    }
+
+    public function getFechaHora(): date
+    {
+        return $this->fechaHora;
+    }
+
+    public function setFechaHora(date $fechaHora)
+    {
+        $this->fechaHora = $fechaHora;
+    }
+
+    public function getLugar(): string
+    {
+        return $this->lugar;
+    }
+
+    public function setLugar(string $lugar)
+    {
+        $this->lugar = $lugar;
+    }
+
+    public function getCaja(): int
+    {
+        return $this->caja;
+    }
+
+    public function setCaja(int $caja)
+    {
+        $this->caja = $caja;
+    }
+
+    public function getPrecio(): int
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio(int $precio)
+    {
+        $this->precio = $precio;
+    }
+
+    public function getDescuento(): bool
+    {
+        return $this->descuento;
+    }
+
+    public function setDescuento(int $descuento)
+    {
+        $this->descuento = $descuento;
+    }
+
+    public function getTotal(): int
+    {
+        return $this->total;
+    }
+
+    public function setTotal(int $total)
+    {
+        $this->total = $total;
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            "id" => $this->id,
+            "nombre" => $this->nombre,
+        ];
+
+        // Esto sería lo mismo:
+        //$array["nombre"] = $this->nombre;
+        //$array["id"] = $this->id;
+        //return $array;
+    }
+
+    public function eliminar(): bool {
+        // Esto sería un control para NO eliminar una categoría si "contiene" personas.
+        if ($this->obtenerPersonasPertenecientes()) return false;
+
+        return DAO::categoriaEliminarPorId($this->id);
+    }
+
+    public function obtenerPersonasPertenecientes(): array
+    {
+        if ($this->personasPertenecientes == null) $personasPertenecientes = DAO::personaObtenerPorCategoria($this->id);
+
+        return $personasPertenecientes;
+    }
+}
