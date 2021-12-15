5a449a28a112e736691006e409ac2a0d43e03128
alainprofe <alain.fernandez@educa.madrid.org>
2021-12-15 10:34:57 +0100

    Cambios de hoy en clase, estamos a medias de hacer la gestión de productos por parte del admin.

---
+++ __Documentacion/Proyecto Tienda.mm
@@ -0,0 +1,105 @@
+<map version="freeplane 1.9.8">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="" FOLDED="false" ID="ID_452131666" CREATED="1610381621610" MODIFIED="1639479572956" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="2.593">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false" associatedTemplateLocation="template:/standard-1.6-noEdgeColor.mm"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_207122690" COLOR="#000000" STYLE="fork">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_207122690" STARTARROW="DEFAULT" ENDARROW="NONE"/>
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+<richcontent CONTENT-TYPE="plain/auto" TYPE="DETAILS"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_3752836">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#003399" TRANSPARENCY="255" DESTINATION="ID_3752836"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="" POSITION="right" ID="ID_899932159" CREATED="1639479568767" MODIFIED="1639479568767">
+<node TEXT="fases" ID="ID_1005422819" CREATED="1639479750519" MODIFIED="1639479751730">
+<node TEXT="partimos de momento estable" ID="ID_1377984847" CREATED="1639479752319" MODIFIED="1639479758137">
+<node TEXT="una sola rama" ID="ID_1016401735" CREATED="1639479759791" MODIFIED="1639479762953"/>
+</node>
+<node TEXT="pensamos próximos pasos" ID="ID_236645203" CREATED="1639479768983" MODIFIED="1639479776169">
+<node TEXT="creamos issues" ID="ID_257850010" CREATED="1639479776391" MODIFIED="1639479779137"/>
+</node>
+<node TEXT="asignamos issues con fecha" ID="ID_518818279" CREATED="1639479780039" MODIFIED="1639479786281"/>
+<node TEXT="N issues" ID="ID_1013741125" CREATED="1639479877151" MODIFIED="1639479879274">
+<node TEXT="para cada issue" ID="ID_250002966" CREATED="1639479871039" MODIFIED="1639479873634">
+<node TEXT="evaluamos cuál es la mejor" ID="ID_820083659" CREATED="1639479786551" MODIFIED="1639479796401">
+<node TEXT="la integramos" ID="ID_1857108540" CREATED="1639479796686" MODIFIED="1639479799617"/>
+</node>
+</node>
+</node>
+<node TEXT="dejar todo en una sola rama" ID="ID_566774444" CREATED="1639479852151" MODIFIED="1639479855386"/>
+<node TEXT="reorganización" ID="ID_1764775858" CREATED="1639479800631" MODIFIED="1639479806233"/>
+<node TEXT="" ID="ID_13304643" CREATED="1639479893943" MODIFIED="1639479898829">
+<icon BUILTIN="up"/>
+</node>
+</node>
+<node TEXT="objs" ID="ID_1610184515" CREATED="1639479569006" MODIFIED="1639479577195">
+<node TEXT="hacer para aprender" ID="ID_725137755" CREATED="1639479578303" MODIFIED="1639479582019"/>
+<node TEXT="construir la app" ID="ID_23871377" CREATED="1639479587598" MODIFIED="1639479590826">
+<node TEXT="¿a suertes?" ID="ID_694406191" CREATED="1639479602103" MODIFIED="1639479630021">
+<icon BUILTIN="button_ok"/>
+</node>
+<node TEXT="pre-elegido" ID="ID_146760412" CREATED="1639479606719" MODIFIED="1639479610658"/>
+</node>
+<node TEXT="evaluar" ID="ID_1545392857" CREATED="1639479638391" MODIFIED="1639479640994"/>
+</node>
+</node>
+</node>
+</map>
---
+++ admin/ProductosGestion.js
@@ -0,0 +1,236 @@
+// TODO Quedaría pendiente poner un timer para actualizar lo local si actualizan el servidor. Una solución óptima sería poner timestamp de modificación en la tabla y pedir elementoqueseaObtenerModificadosDesde(timestamp), donde timestamp es la última vez que he pedido algo.
+
+
+
+window.onload = inicializar;
+
+
+
+// ---------- VARIOS DE BASE/UTILIDADES ----------
+
+function notificarUsuario(texto) {
+    // TODO En lugar del alert, habría que añadir una línea en una zona de notificaciones, arriba, con un temporizador para que se borre solo en ¿5? segundos.
+    alert(texto);
+}
+
+// TODO El parametro "parametros" podía ser directamente un objeto JS y lo convertimos dentro de esta función, en lugar de que lo convierta desde fuera el llamante.
+function llamadaAjax(url, parametros, manejadorOK, manejadorError) {
+    //TODO PARA DEPURACIÓN: alert("Haciendo ajax a " + url + "\nCon parámetros " + parametros);
+
+    var request = new XMLHttpRequest();
+
+    request.open("POST", url);
+    request.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    request.onreadystatechange = function() {
+        if (this.readyState == 4) { // 4 equivale a "hemos terminado".
+            if (request.status == 200) { // 200 significa "todo bien".
+                manejadorOK(request.responseText);
+            } else {
+                if (manejadorError != null) manejadorError(request.responseText);
+            }
+        }
+    };
+
+    request.send(parametros);
+}
+
+function extraerId(texto) {
+    return texto.split('-')[1];
+}
+
+function objetoAParametrosParaRequest(objeto) {
+    // Esto convierte un objeto JS en un listado de clave1=valor1&clave2=valor2&clave3=valor3
+    return new URLSearchParams(objeto).toString();
+}
+
+function debug() {
+    // Esto es útil durante el desarrollo para programar el disparado de acciones concretas mediante un simple botón.
+}
+
+
+
+// ---------- MANEJADORES DE EVENTOS / COMUNICACIÓN CON PHP ----------
+
+// TODO Estaría genial que estos métodos no metieran la mano para nada en el DOM, sino que lo hicieran todo a través de métodos domTalCosa:
+// Por ejemplo: disablearCamposPersonaCrear(), enablearCamposPersonaCrear(), obtenerObjetoPersonaDeCamposPersonaCrear()...
+
+function inicializar() {
+    btnCrear.onclick = clickCrear;
+
+    // En los "Insertar" de a continuación no se fuerza la ordenación, ya que PHP
+    // nos habrá dado los elementos en orden correcto y sería una pérdida de tiempo.
+
+    llamadaAjax("ProductoObtenerTodos.php", "",
+        function(texto) {
+            var productos = JSON.parse(texto);
+
+            for (var i=0; i<productos.length; i++) {
+                domInsertar(productos[i]);
+            }
+        },
+        function(texto) {
+            notificarUsuario("Error Ajax al cargar categorías al inicializar: " + texto);
+        }
+    );
+}
+
+function clickCrear() {
+    inpNombre.disabled = true;
+
+    llamadaAjax("ProductoCrear.php", "nombre=" + inpNombre.value,
+        function(texto) {
+            // Se re-crean los datos por si han modificado/normalizado algún valor en el servidor.
+            var producto = JSON.parse(texto);
+
+            // Se fuerza la ordenación, ya que este elemento podría no quedar ordenado si se pone al final.
+            domInsertar(producto, true);
+
+            inpNombre.value = "";
+            inpNombre.disabled = false;
+        },
+        function(texto) {
+            notificarUsuario("Error Ajax al crear: " + texto);
+            inpNombre.disabled = false;
+        }
+    );
+}
+
+function blurModificar(input) {
+    let div = input.parentElement.parentElement;
+    let producto = domDivAObjeto(div);
+
+    llamadaAjax("ProductoActualizar.php", objetoAParametrosParaRequest(producto),
+        function(texto) {
+            if (texto != "null") {
+                // Se re-crean los datos por si han modificado/normalizado algún valor en el servidor.
+                producto = JSON.parse(texto);
+                domModificar(producto);
+            } else {
+                notificarUsuario("Error Ajax al modificar: " + texto);
+            }
+        },
+        function(texto) {
+            notificarUsuario("Error Ajax al modificar: " + texto);
+        }
+    );
+}
+
+function clickEliminar(id) {
+    llamadaAjax("ProductoEliminar.php", "id="+id,
+        function(texto) {
+            var operacionOK = JSON.parse(texto);
+            if (operacionOK) {
+                domEliminar(id);
+            } else {
+                notificarUsuario("Error Ajax al eliminar: " + texto);
+            }
+        },
+        function(texto) {
+            notificarUsuario("Error Ajax al eliminar: " + texto);
+        }
+    );
+}
+
+
+
+// ---------- GESTIÓN DEL DOM ----------
+
+function domCrearDivInputText(textoValue, codigoOnblur) {
+    let div = document.createElement("div");
+        let input = document.createElement("input");
+                input.setAttribute("type", "text");
+                input.setAttribute("value", textoValue);
+                input.setAttribute("onblur", codigoOnblur + " return false;");
+    div.appendChild(input);
+
+    return div;
+}
+
+function domCrearDivImg(urlSrc, codigoOnclick) {
+    let div = document.createElement("div");
+        let img = document.createElement("img");
+                img.setAttribute("src", urlSrc);
+                img.setAttribute("onclick", codigoOnclick + " return false;");
+    div.appendChild(img);
+
+    return div;
+}
+
+
+
+function domObjetoADiv(producto) {
+    let div = document.createElement("div");
+            div.setAttribute("id", "producto-" + producto.id);
+    div.appendChild(domCrearDivInputText(producto.nombre, "blurModificar(this);"));
+    div.appendChild(domCrearDivImg("../_img/Eliminar.png", "clickEliminar(" + producto.id + ");"));
+
+    return div;
+}
+
+function domObtenerDiv(pos) {
+    return divDatos.children[pos];
+}
+
+function domDivAObjeto(div) {
+    return { // Devolvemos un objeto recién creado con los datos que hemos obtenido.
+        "id": extraerId(div.id),
+        "nombre": div.children[0].children[0].value,
+    };
+}
+
+function domObtenerObjeto(pos) {
+    let div = domObtenerDiv(pos);
+    return domDivAObjeto(div);
+}
+
+function domEjecutarInsercion(pos, producto) {
+    let divReferencia = domObtenerDiv(pos);
+    let divNuevo = domObjetoADiv(producto);
+
+    divDatos.insertBefore(divNuevo, divReferencia);
+}
+
+function domInsertar(productoNueva, enOrden=false) {
+    // Si piden insertar en orden, se buscará su lugar. Si no, irá al final.
+    if (enOrden) {
+        for (let pos=0; pos < divDatos.children.length; pos++) {
+            let productoActual = domObtenerObjeto(pos);
+
+            if (productoNueva.nombre.localeCompare(productoActual.nombre) == -1) {
+                // Si la categoría nueva va ANTES que la actual, este es el punto en el que insertarla.
+                domEjecutarInsercion(pos, productoNueva);
+                return;
+            }
+        }
+    }
+
+    // Si llegamos hasta aquí, insertamos al final.
+    domEjecutarInsercion(divDatos.children.length, productoNueva);
+}
+
+function domLocalizarPosicion(idBuscado) {
+    var divs = divDatos.children;
+
+    for (var pos=0; pos < divs.length; pos++) {
+        let div = divs[pos];
+        let productoActualId = extraerId(div.id);
+
+        if (productoActualId == idBuscado) return (pos);
+    }
+
+    return -1;
+}
+
+function domEliminar(id) {
+    let pos = domLocalizarPosicion(id);
+    let div = domObtenerDiv(pos);
+    div.remove();
+}
+
+function domModificar(producto) {
+    domEliminar(producto.id);
+
+    // Se fuerza la ordenación, ya que este elemento podría no quedar ordenado si se pone al final.
+    domInsertar(producto, true);
+}
