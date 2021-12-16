3e798bbc1b01b33443ead6354265335e5b2fb064
AdrianCardin <acardinlozano@gmail.com>
2021-12-16 14:03:44 +0100

    hecho trazas ( alfa )

--- admin/ProductoActualizar.php
+++ admin/ProductoActualizar.php
@@ -3,6 +3,11 @@
 require_once "../_com/__RequireOnceComunes.php";
 
 $categoria = new Categoria($_REQUEST["id"], $_REQUEST["nombre"]);
+$traza=new Traza(1,"ProductoActualizar" , "Se ha actualizado un producto ", 0 ,date("F j, Y, g:i a"));
+// OJO ----> en el 4 campo del constructor debe ponerse el id del creado ( $categoria->getId() ) , demomento no pongo asi porq es una beta
+
+
+DAO::registrarAccion($traza);
 
 $categoria = DAO::categoriaActualizar($categoria);
 
--- admin/ProductoObtenerTodos.php
+++ admin/ProductoObtenerTodos.php
@@ -3,8 +3,9 @@
 require_once "../_com/__RequireOnceComunes.php";
 
 $resultado = DAO::productoObtenerTodos();
-$traza=new Traza(1,"obtenerTodosProductos","Se ha obtenido todos los productos",0,date("F j, Y, g:i a"));
+$traza=new Traza(1,"ProductoCrear","Se ha creado un producto ", 0 , date("F j, Y, g:i a"));
+// OJO ----> en el 4 campo del constructor debe ponerse el id del creado ( $categoria->getId() ) , demomento no pongo asi porq es una beta
+
 DAO::registrarAccion($traza);
 
-echo ("_________________________");
 echo json_encode($resultado);
